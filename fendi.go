50e2121484b5ace4f9c237f6dece4893086f97e5
fendiirfan <56022249+fendiirfan@users.noreply.github.com>
2019-11-14 11:55:08 +0700

    Add files via upload

---
+++ fendi.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//record login costumer service
+type data struct[
+	var nik,nama,jenis string
+	var setoranawal int
+]
+//
+
+//
+
+func main (){
+	// Input NIK, nama, jenis rekening, dan setoran awal.
+	fmt.Print("NIK : ")
+	fmt.Scanln(&data.nik)
+	for data.nik != len nik {
+		fmt,Println("nik tidak valid mohon input kembali !!! ")
+		fmt.Print("NIK : ")
+		fmt.Scanln(&data.nik)		
+		} 
+	fmt.Print("NAMA : ")
+	fmt.Scanln(&data.nik)
+	fmt.Print("JENIS REKENING : ")
+	fmt.Scanln(&data.jenis)
+	fmt.Print("SETORAN AWAL : ")
+	fmt.Scanln(&data.setoranawal)
+	//
+
+
+
+}
